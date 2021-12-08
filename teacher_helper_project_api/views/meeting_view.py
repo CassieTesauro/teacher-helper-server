48ac0d53d042dcc4861613c9a90a2fb493f919f3
CassieTesauro <cassie.tesauro@gmail.com>
2021-12-08 14:44:00 -0600

    meetingview retrieve method works

--- teacher_helper_project_api/views/meeting_view.py
+++ teacher_helper_project_api/views/meeting_view.py
@@ -16,7 +16,7 @@ from django.contrib.auth.models import User
 class MeetingView(ViewSet):  
     """current user's meetings"""
 
-    #get list of all students 
+    #get list of all meetings 
     def list(self, request):
         """Handle GET requests to meetings resource
         Returns:
@@ -27,10 +27,14 @@ class MeetingView(ViewSet):
         current_user = request.auth.user
 
 
-        #get all Meetings, then get all meetings with current user's pk as a foreign key
+        #get all Meetings 
         meetings = Meeting.objects.all()   
+            
+
+
+        #get all meetings with current user's pk as a foreign key
         if meetings is not None:
-            current_user_meetings = meetings.filter(user_id=current_user)  # section in filter is (column in student table :: what we're matching to in filter)
+            current_user_meetings = meetings.filter(user_id=current_user)  # section in filter is (column in sql table :: what we're matching to in filter)
 
 
         #translate to JSON and respond to client side
@@ -38,6 +42,45 @@ class MeetingView(ViewSet):
             current_user_meetings, many=True, context={'request': request}) 
 
         return Response(meetings_serializer.data) 
+    
+
+
+    #get single meeting with students assigned to meeting
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single meeting
+        Returns:
+            Response -- JSON serialized meeting instance
+        """
+        try:
+            #get current meeting using pk
+            requested_meeting = Meeting.objects.get(pk=pk)
+
+            #translate meeting JSON 
+            meeting_serializer = MeetingsSerializer(
+                requested_meeting, context={'request': request})
+
+            
+        
+  
+
+            return Response(meeting_serializer.data)
+       
+       
+       
+        except Exception as ex:  
+            return HttpResponseServerError(ex)
+
+
+#make serializer for students
+class StudentsSerializer(serializers.ModelSerializer):
+    """JSON serializer for students
+    Arguments:
+        serializer type
+    """
+    class Meta:
+        model = Student
+        fields = ('id', 'name')
+        depth = 1
 
 
 #make serializer for meetings
@@ -46,7 +89,10 @@ class MeetingsSerializer(serializers.ModelSerializer):
     Arguments:
         serializer type
     """
+  
+
     class Meta:
         model = Meeting
-        fields = ('id', 'name', 'description', 'date', 'time')
+        fields = ('id', 'name', 'description', 'date', 'time', 'learners')
         depth = 1
+
